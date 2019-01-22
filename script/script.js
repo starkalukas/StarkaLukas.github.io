8ecdfa0b0da718c127288529c53ebd1afd961c64
Starka Lukas <it170172@EDU.HTL-LEONDING.AC.AT>
2019-01-22 17:13:22 +0100

    GITHUB REFERENCE

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let github = document.getElementById('github');
+
+github.addEventListener('click', clickAnimation);
+
+function clickAnimation(){
+    window.open("https://github.com/StarkaLukas");
+}
