bfddfa16971ef3551e965b494387b6d9dcafe865
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-01-28 00:02:24 +0100

    added Pokemon

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
 let github = document.getElementById('github');
 
 github.addEventListener('click', clickAnimation);
+document.getElementById('pokeball').addEventListener('click', openPokemon)
+
 
 function clickAnimation(){
-    window.open("https://github.com/StarkaLukas");
+    window.open('https://github.com/StarkaLukas', '_self');
+}
+function openPokemon(){
+    window.open('./MEDT_Sammelspiel/index.html', '_self');
 }
