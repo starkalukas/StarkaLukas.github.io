50d6f6d21971b4e43e41e242dbac9959f447e1d6
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-01-28 08:52:52 +0100

    added Pokemon

--- script/script.js
+++ script/script.js
@@ -8,5 +8,5 @@ function clickAnimation(){
     window.open('https://github.com/StarkaLukas', '_self');
 }
 function openPokemon(){
-    window.open('./MEDT_Sammelspiel/index.html', '_self');
+    window.open('https://starkalukas.github.io/Pokemon/', '_self');
 }
