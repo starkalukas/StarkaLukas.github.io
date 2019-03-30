f5c57a071c75a1bd0573cb09ba9584dfc7b38fca
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-03-30 19:08:23 +0100

    more links available

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
-setTimeout(link, 5000);
+document.getElementById('button1').addEventListener('click', click1);
+document.getElementById('button2').addEventListener('click', click2);
+document.getElementById('button3').addEventListener('click', click3);
+document.getElementById('button4').addEventListener('click', click4);
 
-function link(){
+function click1(){
     window.open('https://starkalukas.github.io/PROJECT', '_self');
+}
+function click2(){
+    window.open('https://starkalukas.github.io/chart.js', '_self');
+}
+function click3(){
+    window.open('https://schollsebastian.github.io', '_self');
+}
+function click4(){
+    window.open('https://github.com/StarkaLukas', '_self');
 }
