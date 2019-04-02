079d97a74d5ef1918ee5eda2482121fdc99c2a8c
StarkaLukas <43465679+StarkaLukas@users.noreply.github.com>
2019-04-02 13:53:00 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,17 +1,19 @@
-document.getElementById('button1').addEventListener('click', click1);
-document.getElementById('button2').addEventListener('click', click2);
-document.getElementById('button3').addEventListener('click', click3);
-document.getElementById('button4').addEventListener('click', click4);
+//document.getElementById('button1').addEventListener('click', click1);
+//document.getElementById('button2').addEventListener('click', click2);
+//document.getElementById('button3').addEventListener('click', click3);
+//document.getElementById('button4').addEventListener('click', click4);
+//
+//function click1(){
+//    window.open('https://starkalukas.github.io/PROJECT', '_self');
+//}
+//function click2(){
+//    window.open('https://starkalukas.github.io/chart.js', '_self');
+//}
+//function click3(){
+//    window.open('https://schollsebastian.github.io', '_self');
+//}
+//function click4(){
+//    window.open('https://github.com/StarkaLukas', '_self');
+// }
 
-function click1(){
-    window.open('https://starkalukas.github.io/PROJECT', '_self');
-}
-function click2(){
-    window.open('https://starkalukas.github.io/chart.js', '_self');
-}
-function click3(){
-    window.open('https://schollsebastian.github.io', '_self');
-}
-function click4(){
-    window.open('https://github.com/StarkaLukas', '_self');
-}
+console.log('DU HOSD DO AN ERROR LUKAS!');
