ae52f39beccdb1e135dc9bc229e594d54de77e66
StarkaLukas <43465679+StarkaLukas@users.noreply.github.com>
2019-10-11 11:18:09 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,19 +1,17 @@
-//document.getElementById('button1').addEventListener('click', click1);
-//document.getElementById('button2').addEventListener('click', click2);
-//document.getElementById('button3').addEventListener('click', click3);
-//document.getElementById('button4').addEventListener('click', click4);
-//
-//function click1(){
-//    window.open('https://starkalukas.github.io/PROJECT', '_self');
-//}
-//function click2(){
-//    window.open('https://starkalukas.github.io/chart.js', '_self');
-//}
-//function click3(){
-//    window.open('https://schollsebastian.github.io', '_self');
-//}
-//function click4(){
-//    window.open('https://github.com/StarkaLukas', '_self');
-// }
+document.getElementById('button1').addEventListener('click', click1);
+document.getElementById('button2').addEventListener('click', click2);
+document.getElementById('button3').addEventListener('click', click3);
+document.getElementById('button4').addEventListener('click', click4);
 
-console.log('DU HOSD DO AN ERROR LUKAS!');
+function click1(){
+    window.open('https://starkalukas.github.io/Dartzone', '_self');
+}
+function click2(){
+    window.open('https://starkalukas.github.io/chart.js', '_self');
+}
+function click3(){
+    window.open('https://schollsebastian.github.io', '_self');
+}
+function click4(){
+    window.open('https://github.com/StarkaLukas', '_self');
+}
